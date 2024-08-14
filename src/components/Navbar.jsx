bb50751f8c6f91a8c1f3d719c6c819070ee6c467
Shalaka <shalakaw17@gmail.com>
2024-08-14 15:21:37 +0530

    popup cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Cart from './Cart'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
-    <div className='flex justify-between items-center bg-pink-300 text-white px-7 py-9 '>
+    <div className='flex justify-between items-center shadow-lg bg-pink-300 text-white px-7 py-9 '>
         <h1 className=' font-bold'>THE ICECREAM SHOP</h1>
         <ul className='flex space-x-10'>
             <li className=' transition-all hover:border-b-4 border-yellow-200 hover:text-pink-600'>All</li>
@@ -13,11 +14,13 @@ const Navbar = () => {
             <li className=' transition-all hover:border-b-4 border-yellow-200 hover:text-pink-600'>Classics</li>
 
         </ul>
-        <div className='space-x-3'>
-        <ShoppingCartIcon/>
-        <Link to={"/Login"} className=' '>LOGIN </Link>
-        <Link to={"/Signup"} className=' '>SIGNUP </Link>
-        </div>
+        <ul className='flex space-x-3'>
+          <li><ShoppingCartIcon/></li>
+          <li><Cart/></li>
+          <li><Link to={"/Login"} className=' '>LOGIN </Link></li>
+          <li><Link to={"/Signup"} className=' '>SIGNUP </Link></li>
+   
+        </ul>
 
 
     </div>
