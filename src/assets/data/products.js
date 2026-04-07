33def4342f47ae08aab30f003716b610f407b49f
Shalaka Wairkar <shalakaw17@gmail.com>
2026-04-07 22:53:34 +0530

    added products data

---
+++ src/assets/data/products.js
@@ -0,0 +1,52 @@
+export const products = [
+  {
+    id: 1,
+    name: "Mango Popsicle",
+    category: "popsicles",
+    price: 60,
+    description: "Fresh alphonso mango on a stick",
+    image: "https://images.unsplash.com/photo-1556679343-c7306c1976bc?w=400"
+  },
+  {
+    id: 2,
+    name: "Chocolate Fudge Scoop",
+    category: "scoops",
+    price: 80,
+    description: "Rich dark chocolate, two scoops",
+    image: "https://images.unsplash.com/photo-1563805042-7684c019e1cb?w=400"
+  },
+  {
+    id: 3,
+    name: "Strawberry Cone",
+    category: "cones",
+    price: 70,
+    description: "Classic strawberry in a waffle cone",
+    image: "https://images.unsplash.com/photo-1501443762994-82bd5dace89a?w=400"
+  },
+  {
+    id: 4,
+    name: "Rainbow Sprinkles",
+    category: "toppings",
+    price: 20,
+    description: "Add a burst of colour to anything",
+    image: "https://images.unsplash.com/photo-1612201001768-421f0f02a17a?w=400"
+  },
+  {
+    id: 5,
+    name: "Kulfi Classic",
+    category: "popsicles",
+    price: 50,
+    description: "Traditional Indian kulfi, cardamom flavour",
+    image: "https://images.unsplash.com/photo-1590080876351-41a3f9f0e0e7?w=400"
+  },
+  {
+    id: 6,
+    name: "Vanilla Dream Scoop",
+    category: "scoops",
+    price: 70,
+    description: "Pure Madagascar vanilla, one scoop",
+    image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400"
+  }
+]
+
+export const categories = ["all", "popsicles", "scoops", "cones", "toppings"]
