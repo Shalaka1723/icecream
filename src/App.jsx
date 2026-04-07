6219ed87d5d1200b4e39b2a29e81a26e14a5dada
Shalaka Wairkar <shalakaw17@gmail.com>
2026-04-07 23:22:30 +0530

    changed folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter,  Route,  Routes } from 'react-router-dom'
-import Home from './components/Home'
-import Login from './components/Login'
-import Singup from './components/Signup'
+import Home from './components/main/Home'
+import Login from './components/common/Login'
+import Singup from './components/common/Signup'
 
 const App = () => {
   return (
